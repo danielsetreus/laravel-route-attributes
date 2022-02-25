67c409130fce2fcaa31d061d51409b83591ae935
Daniel Setréus <daniel.setreus@gmail.com>
2022-02-25 14:09:32 +0100

    Debugging - exclude all but failing test

--- tests/RouteRegistrarTest.php
+++ tests/RouteRegistrarTest.php
@@ -11,60 +11,60 @@ use ThirdParty\Http\Controllers\ThirdPartyController;
 class RouteRegistrarTest extends TestCase
 {
     /** @test */
-    public function the_registrar_can_register_a_single_file()
-    {
-        $this
-            ->routeRegistrar
-            ->registerFile($this->getTestPath('TestClasses/Controllers/RouteRegistrar/RegistrarTestFirstController.php'));
+    // public function the_registrar_can_register_a_single_file()
+    // {
+    //     $this
+    //         ->routeRegistrar
+    //         ->registerFile($this->getTestPath('TestClasses/Controllers/RouteRegistrar/RegistrarTestFirstController.php'));
 
-        $this->assertRegisteredRoutesCount(1);
+    //     $this->assertRegisteredRoutesCount(1);
 
-        $this->assertRouteRegistered(
-            RegistrarTestFirstController::class,
-            uri: 'first-method',
-        );
-    }
-
-    /** @test */
-    public function the_registrar_can_apply_config_middlewares_to_all_routes()
-    {
-        $this
-            ->routeRegistrar
-            ->registerFile($this->getTestPath('TestClasses/Controllers/RouteRegistrar/RegistrarTestFirstController.php'));
-
-        $this->assertRegisteredRoutesCount(1);
-
-        $this->assertRouteRegistered(
-            RegistrarTestFirstController::class,
-            uri: 'first-method',
-            middleware: [AnotherTestMiddleware::class]
-        );
-    }
+    //     $this->assertRouteRegistered(
+    //         RegistrarTestFirstController::class,
+    //         uri: 'first-method',
+    //     );
+    // }
 
     /** @test */
-    public function the_registrar_can_register_a_whole_directory()
-    {
-        $this
-            ->routeRegistrar
-            ->registerDirectory($this->getTestPath('TestClasses/Controllers/RouteRegistrar'));
+    // public function the_registrar_can_apply_config_middlewares_to_all_routes()
+    // {
+    //     $this
+    //         ->routeRegistrar
+    //         ->registerFile($this->getTestPath('TestClasses/Controllers/RouteRegistrar/RegistrarTestFirstController.php'));
 
-        $this->assertRegisteredRoutesCount(3);
+    //     $this->assertRegisteredRoutesCount(1);
 
-        $this->assertRouteRegistered(
-            RegistrarTestFirstController::class,
-            uri: 'first-method',
-        );
+    //     $this->assertRouteRegistered(
+    //         RegistrarTestFirstController::class,
+    //         uri: 'first-method',
+    //         middleware: [AnotherTestMiddleware::class]
+    //     );
+    // }
 
-        $this->assertRouteRegistered(
-            RegistrarTestSecondController::class,
-            uri: 'second-method',
-        );
+    /** @test */
+    // public function the_registrar_can_register_a_whole_directory()
+    // {
+    //     $this
+    //         ->routeRegistrar
+    //         ->registerDirectory($this->getTestPath('TestClasses/Controllers/RouteRegistrar'));
 
-        $this->assertRouteRegistered(
-            RegistrarTestControllerInSubDirectory::class,
-            uri: 'in-sub-directory',
-        );
-    }
+    //     $this->assertRegisteredRoutesCount(3);
+
+    //     $this->assertRouteRegistered(
+    //         RegistrarTestFirstController::class,
+    //         uri: 'first-method',
+    //     );
+
+    //     $this->assertRouteRegistered(
+    //         RegistrarTestSecondController::class,
+    //         uri: 'second-method',
+    //     );
+
+    //     $this->assertRouteRegistered(
+    //         RegistrarTestControllerInSubDirectory::class,
+    //         uri: 'in-sub-directory',
+    //     );
+    // }
 
     /** @test */
     public function the_register_can_register_a_directory_with_defined_namespace()
